cb0d8195152637562fa96971488fff1905251711
Sergej <sergey.gavrilov97@mail.ru>
2016-12-28 01:52:17 +0300

    MAps added

--- app/src/main/java/ru/luvas/dk/client/MapsActivity.java
+++ app/src/main/java/ru/luvas/dk/client/MapsActivity.java
@@ -1,12 +1,28 @@
 package ru.luvas.dk.client;
 
+import android.*;
+import android.Manifest;
 import android.app.Activity;
 import android.app.Dialog;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+import android.location.Address;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+//import com.google.android.gms.identity.intents.Address;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,26 +30,105 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 
+
+import java.io.IOException;
+import java.util.List;
+
+import ru.luvas.dk.client.utils.Recognizer;
+
 /**
  * Created by sergey on 27.12.16.
  */
 
-public class MapsActivity extends Activity implements OnMapReadyCallback {
+public class MapsActivity extends Activity
+        implements OnMapReadyCallback, GoogleApiClient.OnConnectionFailedListener,
+        GoogleApiClient.ConnectionCallbacks {
 
 
-    GoogleMap mGoogleMap;
+    private GoogleMap mGoogleMap;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    public final static int PERMISSIONS_REQUEST_ID = 7661;
+
+    private Recognizer recognizer;
+    private Button bListen;
+    private EditText mEditText;
+    public static MapsActivity lastInstance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         if (googleServiceAvailable()) {
             setContentView(R.layout.maps_layout);
             initMap();
         }
+
+        lastInstance = this;
+        //  GoogleApiClient mGoogleApiClient = new GoogleApiClient.Builder(this)
+        //        .addApi(LocationServices.API)
+        //      .build();;
+        recognizer = new Recognizer(this);
+        bListen = (Button) findViewById(R.id.buttonListen);
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addApi(LocationServices.API)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .build();
+        }
+
+        mEditText = (EditText) findViewById(R.id.editText);
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mapTypeNone:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+                break;
+            case R.id.mapTypeNormal:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                break;
+            case R.id.mapTypeSatellite:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                break;
+            case R.id.mapTypeTerrain:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                break;
+            case R.id.mapTypeHybrid:
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                break;
+
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+
     private void initMap() {
-        MapFragment mapFragment = (MapFragment)getFragmentManager().findFragmentById(R.id.mapFragment);
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
         mapFragment.getMapAsync(this);
     }
 
@@ -53,7 +148,7 @@ public class MapsActivity extends Activity implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        
+
         mGoogleMap = googleMap;
         goToLocation(29, -70);
     }
@@ -64,9 +159,71 @@ public class MapsActivity extends Activity implements OnMapReadyCallback {
         mGoogleMap.moveCamera(update);
     }
 
+    public void askForPermissions(Activity activity) {
+        ActivityCompat.requestPermissions(activity,
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                PERMISSIONS_REQUEST_ID);
+    }
+
     private void goToLocationZoom(double lattitude, double longitude, float zoom) {
         LatLng ll = new LatLng(lattitude, longitude);
         CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, zoom);
         mGoogleMap.moveCamera(update);
     }
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+  /*      mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+            mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
+            mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+        }*/
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    public void geoLocate(String location) throws IOException {
+
+        Geocoder gc = new Geocoder(this);
+        List<Address> list = gc.getFromLocationName(location, 1);
+        Address address = list.get(0);
+        String locality = address.getLocality();
+
+        Toast.makeText(this, locality, Toast.LENGTH_LONG).show();
+
+        double lattitude = address.getLatitude();
+        double longitude = address.getLongitude();
+        goToLocationZoom(lattitude, longitude, 16);
+    }
+
+
+    public void geoLocateButton(View view) throws IOException {
+
+        String location = mEditText.getText().toString();
+        geoLocate(location);
+
+    }
+
+    public void geoLocateSpeach(View view) {
+        recognizer.startListening();
+    }
+
+    public void handleInput(String text) throws IOException {
+
+        mEditText.setText(text);
+        geoLocate(text);
+
+    }
+
+
 }
